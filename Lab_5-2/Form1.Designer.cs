d0a8739bdcf205c487d6f215abfc86debd148f77
juraGR2025 <93720304+juraGR2025@users.noreply.github.com>
2022-01-17 22:10:10 +0300

    Variant_2_17-01-2022

---
+++ Lab_5-2/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Lab_5_2
+{
+    partial class RhombForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(343, 195);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(100, 40);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // RhombForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RhombForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.RhombForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ Lab_5-2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_5_2
+{
+    public partial class RhombForm : Form
+    {
+        public RhombForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RhombForm_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath =
+new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point((int)this.Width/2, 0),
+        new Point(0, (int)this.Height/2),
+new Point((int)this.Width/2, this.Height),
+new Point(this.Width, (int)this.Height/2) });
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+    }
+}
