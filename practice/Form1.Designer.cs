12fe0a79a15a779854ddcdb2d12fe2b955edb6a1
juraGR2025 <93720304+juraGR2025@users.noreply.github.com>
2022-01-20 21:00:53 +0300

    Add files via upload

---
+++ practice/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace practice
+{
+    partial class DBconnect
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbResultData = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(271, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbResultData
+            // 
+            this.lbResultData.FormattingEnabled = true;
+            this.lbResultData.Location = new System.Drawing.Point(456, 24);
+            this.lbResultData.Name = "lbResultData";
+            this.lbResultData.Size = new System.Drawing.Size(392, 238);
+            this.lbResultData.TabIndex = 2;
+            // 
+            // DBconnect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 281);
+            this.Controls.Add(this.lbResultData);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "DBconnect";
+            this.Text = "connecting to the Database";
+            this.Load += new System.EventHandler(this.DBconnect_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lbResultData;
+    }
+}
+
---
+++ practice/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice
+{
+    public partial class DBconnect : Form
+    {
+        public DBconnect()
+        {
+            InitializeComponent();
+        }
+
+        private void DBconnect_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=AdventureWorks2017;Data Source=."))//Создание объекта для соединения.                                                                  
+            { //Вносим в него Connection string...
+                cn.Open();//Открываем соединение с базой данных...
+                using (SqlCommand cmd = new SqlCommand("SELECT DepartmentID, GroupName FROM AdventureWorks2017.HumanResources.Department", cn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                           // MessageBox.Show(reader[0] + " " + reader[1]);
+                            lbResultData.Items.Add(reader[0] + " " + reader[1]);
+                        }
+                    }
+                }
+            }
+          
+        }
+    }
+}
